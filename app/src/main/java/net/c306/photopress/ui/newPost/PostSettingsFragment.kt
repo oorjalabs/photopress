650b0a7321a1aa9bd079af8978fc3d003ea675c1
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-08-11 12:43:50 +0100

    Migrate `PostSettingsFragment` from data binding to view binding

--- app/src/main/java/net/c306/photopress/ui/newPost/PostSettingsFragment.kt
+++ app/src/main/java/net/c306/photopress/ui/newPost/PostSettingsFragment.kt
@@ -11,45 +11,33 @@ import android.widget.MultiAutoCompleteTextView
 import android.widget.TextView
 import androidx.annotation.IdRes
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import net.c306.photopress.R
 import net.c306.photopress.databinding.FragmentPostSettingsBinding
 import net.c306.photopress.ui.custom.AppBarNoBottomNavFragment
 import net.c306.photopress.utils.setInputFocus
+import net.c306.photopress.utils.viewBinding
 
-class PostSettingsFragment: AppBarNoBottomNavFragment() {
+class PostSettingsFragment : AppBarNoBottomNavFragment(R.layout.fragment_post_settings) {
     
-    private lateinit var binding: FragmentPostSettingsBinding
+    private val binding by viewBinding(FragmentPostSettingsBinding::bind)
     
     @IdRes
     override val myNavId: Int = R.id.post_settings_fragment
     
-    private val newPostViewModel by activityViewModels<NewPostViewModel>()
-
+    private val viewModel by activityViewModels<NewPostViewModel>()
+    
     private val mTagsAdapter by lazy {
         TagsAutocompleteAdapter(
             context = requireContext(),
-            list = newPostViewModel.blogTags.value?.map { it.name }?.toMutableList()
-                ?: mutableListOf()
+            list = viewModel.blogTags.value?.map { it.name }?.toMutableList()
+                   ?: mutableListOf()
         )
-    }
-    
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentPostSettingsBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewmodel = newPostViewModel
-        binding.handler = Handler()
-        return binding.root
     }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    
+        
         binding.inputPostTags.apply {
             
             setAdapter(mTagsAdapter)
@@ -59,49 +47,56 @@ class PostSettingsFragment: AppBarNoBottomNavFragment() {
                 (v as EditText).setInputFocus(hasFocus, R.string.post_settings_hint_tags)
             }
         }
-    
-        // Update tags in tags suggester
-        newPostViewModel.blogTags.observe(viewLifecycleOwner, Observer { list ->
-            mTagsAdapter.setList(list?.map { it.name } ?: emptyList())
-        })
         
+        // Update tags in tags suggester
+        viewModel.blogTags.observe(viewLifecycleOwner) { list ->
+            mTagsAdapter.setList(list?.map { it.name } ?: emptyList())
+        }
+        
+        viewModel.postTags.observe(viewLifecycleOwner) {
+            binding.inputPostTags.setText(it.orEmpty())
+        }
+        
+        viewModel.postCategoriesDisplayString.observe(viewLifecycleOwner) {
+            binding.categories.text = it ?: getString(R.string.string_none)
+        }
+    
         // Close fragment without saving changes
         binding.toolbar.setNavigationOnClickListener { dismiss() }
+        binding.buttonDone.setOnClickListener { done() }
+        binding.categoriesCollect.setOnClickListener { openCategoriesPicker() }
     }
     
     
-    @Suppress("UNUSED_PARAMETER")
-    inner class Handler {
-        /**
-         * Save values to real variables and close fragment
-         */
-        fun done(view: View) {
-            dismiss()
-        }
+    /**
+     * Save values to real variables and close fragment
+     */
+    private fun done() {
+        dismiss()
+    }
     
-        fun openCategoriesPicker(view: View) {
-            findNavController().navigate(PostSettingsFragmentDirections.actionOpenCategoryPicker())
-        }
+    private fun openCategoriesPicker() {
+        findNavController().navigate(PostSettingsFragmentDirections.actionOpenCategoryPicker())
     }
     
     
     class TagsAutocompleteAdapter(context: Context, list: MutableList<String>) :
         ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, list) {
-
+        
         private val mInflater = LayoutInflater.from(context)
         private val layoutResource = android.R.layout.simple_list_item_1
-
+        
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
             return (convertView ?: mInflater.inflate(layoutResource, null)).apply {
-
+                
                 val wpTag = getItem(position)
-
+                
                 findViewById<TextView>(android.R.id.text1)?.apply {
                     text = wpTag
                 }
             }
         }
-
+        
         fun setList(value: List<String>) {
             clear()
             addAll(value)
