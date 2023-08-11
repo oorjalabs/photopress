764bb3201d45ca49b06ea912ce85b6d2c1ea943b
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-08-11 11:38:44 +0100

    Migrate `CreditsFragment` from data binding to view binding

--- app/src/main/java/net/c306/photopress/ui/CreditsFragment.kt
+++ app/src/main/java/net/c306/photopress/ui/CreditsFragment.kt
@@ -2,55 +2,37 @@ package net.c306.photopress.ui
 
 import android.os.Bundle
 import android.text.Html
-import android.text.Spanned
 import android.text.method.LinkMovementMethod
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.IdRes
 import net.c306.photopress.R
 import net.c306.photopress.databinding.FragmentCreditsBinding
 import net.c306.photopress.ui.custom.AppBarFragment
+import net.c306.photopress.utils.viewBinding
 
 /**
  * Shows credits and thanks for content used
  */
-class CreditsFragment : AppBarFragment() {
+class CreditsFragment : AppBarFragment(R.layout.fragment_credits) {
     
     @IdRes
     override val myNavId: Int = R.id.creditsFragment
     
-    private lateinit var binding: FragmentCreditsBinding
-    
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentCreditsBinding.inflate(inflater, container, false)
-        binding.presenter = Presenter()
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
-    }
-    
+    private val binding by viewBinding { FragmentCreditsBinding.bind(it) }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         
         binding.tvCredits.movementMethod = LinkMovementMethod()
-    
+        
         /**
          * Close fragment without saving changes
          */
         binding.toolbar.setNavigationOnClickListener { dismiss() }
+        
+        binding.tvCredits.text = Html.fromHtml(
+            getString(R.string.credits_full_text),
+            Html.FROM_HTML_MODE_LEGACY
+        ) 
     }
-    
-    inner class Presenter {
-        val creditText: Spanned by lazy {
-            Html.fromHtml(
-                getString(R.string.credits_full_text),
-                Html.FROM_HTML_MODE_LEGACY
-            )
-        }
-    }
-    
 }
--- app/src/main/java/net/c306/photopress/ui/custom/BottomNavFragment.kt
+++ app/src/main/java/net/c306/photopress/ui/custom/BottomNavFragment.kt
@@ -2,13 +2,17 @@ package net.c306.photopress.ui.custom
 
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.LayoutRes
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.c306.photopress.MainActivity
 import net.c306.photopress.R
 
-open class BottomNavFragment: Fragment() {
+open class BottomNavFragment: Fragment {
+    constructor(): super()
+    constructor(@LayoutRes layout: Int): super(layout)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
