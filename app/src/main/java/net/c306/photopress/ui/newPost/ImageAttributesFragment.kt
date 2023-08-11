0ce3ee69c781065a76ed1f53e8df2a15d2bb502d
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-08-11 12:13:59 +0100

    Migrate `ImageAttributesFragment` from data binding to view binding

--- app/src/main/java/net/c306/photopress/ui/newPost/ImageAttributesFragment.kt
+++ app/src/main/java/net/c306/photopress/ui/newPost/ImageAttributesFragment.kt
@@ -1,39 +1,27 @@
 package net.c306.photopress.ui.newPost
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.IdRes
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import net.c306.photopress.R
 import net.c306.photopress.databinding.FragmentImageAttributesBinding
 import net.c306.photopress.ui.custom.AppBarNoBottomNavFragment
+import net.c306.photopress.ui.custom.BindingAdapters
+import net.c306.photopress.utils.viewBinding
 
-class ImageAttributesFragment: AppBarNoBottomNavFragment() {
+class ImageAttributesFragment : AppBarNoBottomNavFragment(R.layout.fragment_image_attributes) {
     
-    private lateinit var binding: FragmentImageAttributesBinding
+    private val binding by viewBinding(FragmentImageAttributesBinding::bind)
     private val args by navArgs<ImageAttributesFragmentArgs>()
     
     @IdRes
     override val myNavId: Int = R.id.imageAttributesFragment
     
-    private val newPostViewModel by activityViewModels<NewPostViewModel>()
-    
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentImageAttributesBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewmodel = newPostViewModel
-        binding.fragmentHandler = Handler()
-        return binding.root
-    }
+    private val viewModel by activityViewModels<NewPostViewModel>()
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,45 +33,69 @@ class ImageAttributesFragment: AppBarNoBottomNavFragment() {
         }
         
         // Set editing image from imageId argument
-        newPostViewModel.postImages.observe(viewLifecycleOwner, Observer {
-            if (it.isNullOrEmpty()) return@Observer
+        viewModel.postImages.observe(viewLifecycleOwner) {
+            if (it.isNullOrEmpty()) return@observe
             
             val editingImage = it.find { image -> image.id == args.imageId }
-                               ?: return@Observer
+                               ?: return@observe
             
-            newPostViewModel.editingImage.value = editingImage
-        })
+            viewModel.editingImage.value = editingImage
+        }
         
+        viewModel.editingImage.observe(viewLifecycleOwner) {
+            BindingAdapters.loadImage(
+                view = binding.image,
+                imageUri = null,
+                imageUriCover = it?.uri,
+                placeHolderDrawable = null,
+            )
+            binding.inputPostTitle.setText(it?.name.orEmpty())
+            binding.inputPostCaption.setText(it?.caption.orEmpty())
+            binding.inputPostAltText.setText(it?.altText.orEmpty())
+            binding.inputPostDescription.setText(it?.description.orEmpty())
+        }
+        
+        viewModel.postFeaturedImageId.observe(viewLifecycleOwner) {
+            val isEditingFeaturedImage = it == viewModel.editingImage.value?.id
+            binding.featuredIndicator.isVisible = isEditingFeaturedImage
+            binding.buttonFeaturedImage.text = getString(
+                if (isEditingFeaturedImage) {
+                    R.string.image_attributes_button_unset_as_featured_image
+                } else {
+                    R.string.image_attributes_button_set_as_featured_image
+                }
+            )
+        }
         /**
          * Close fragment without saving changes
          */
         binding.toolbar.setNavigationOnClickListener { dismiss() }
+        binding.buttonDone.setOnClickListener { done() }
+        binding.image.setOnClickListener { openFullImage() }
+        binding.buttonFeaturedImage.setOnClickListener { viewModel.toggleFeaturedImage(viewModel.editingImage.value?.id) }
+        binding.buttonRemoveImage.setOnClickListener { removeImage() }
     }
     
-    @Suppress("UNUSED_PARAMETER")
-    inner class Handler {
-        /**
-         * Save values to real variables and close fragment
-         */
-        fun done(view: View) {
-            newPostViewModel.editingImage.value?.also {
-                newPostViewModel.updatePostImage(it)
-            }
-            dismiss()
+    /**
+     * Save values to real variables and close fragment
+     */
+    private fun done() {
+        viewModel.editingImage.value?.also {
+            viewModel.updatePostImage(it)
         }
-        
-        fun removeImage(view: View) {
-            newPostViewModel.editingImage.value?.also {
-                newPostViewModel.removeImage(it)
-            }
-            dismiss()
-        }
-        
-        fun openFullImage(view: View) {
-            newPostViewModel.editingImage.value?.also {
-                findNavController().navigate(ImageAttributesFragmentDirections.actionOpenFullPhoto(it))
-            }
-        }
+        dismiss()
     }
     
+    private fun removeImage() {
+        viewModel.editingImage.value?.also {
+            viewModel.removeImage(it)
+        }
+        dismiss()
+    }
+    
+    private fun openFullImage() {
+        viewModel.editingImage.value?.also {
+            findNavController().navigate(ImageAttributesFragmentDirections.actionOpenFullPhoto(it))
+        }
+    }
 }
