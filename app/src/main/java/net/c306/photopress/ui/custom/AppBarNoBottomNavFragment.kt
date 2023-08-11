80c51a5c21f15f87856ade784c43258c9dccf5cf
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-08-11 11:59:31 +0100

    Migrate `ReorderImagesFragment` from data binding to view binding

--- app/src/main/java/net/c306/photopress/ui/custom/AppBarNoBottomNavFragment.kt
+++ app/src/main/java/net/c306/photopress/ui/custom/AppBarNoBottomNavFragment.kt
@@ -1,9 +1,13 @@
 package net.c306.photopress.ui.custom
 
+import androidx.annotation.LayoutRes
 import androidx.navigation.fragment.findNavController
 import net.c306.customcomponents.utils.hideKeyboard
 
-abstract class AppBarNoBottomNavFragment: NoBottomNavFragment(), AppBarFragmentInterface {
+abstract class AppBarNoBottomNavFragment: NoBottomNavFragment, AppBarFragmentInterface {
+    
+    constructor(): super()
+    constructor(@LayoutRes layout: Int): super(layout)
     
     abstract override val myNavId: Int
     
