ecd658f37a2c197917951c05c74aa84499105156
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-08-11 10:32:00 +0100

    Migrate `WelcomeFragment` from data binding to view binding

--- app/src/main/java/net/c306/photopress/ui/custom/NoBottomNavFragment.kt
+++ app/src/main/java/net/c306/photopress/ui/custom/NoBottomNavFragment.kt
@@ -2,12 +2,16 @@ package net.c306.photopress.ui.custom
 
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.c306.photopress.MainActivity
 import net.c306.photopress.R
 
-open class NoBottomNavFragment: Fragment() {
+open class NoBottomNavFragment: Fragment {
+    
+    constructor(): super()
+    constructor(@LayoutRes layout: Int): super(layout)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
