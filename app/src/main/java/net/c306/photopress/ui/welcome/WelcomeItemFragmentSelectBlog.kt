927011f71efd7d5fba6cd5b49496570d1da5284f
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2024-02-11 20:20:33 +0000

    Update BlogChooserDialogFragment to use Fragment result API

--- app/src/main/java/net/c306/photopress/ui/welcome/WelcomeItemFragmentSelectBlog.kt
+++ app/src/main/java/net/c306/photopress/ui/welcome/WelcomeItemFragmentSelectBlog.kt
@@ -5,6 +5,8 @@ import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import net.c306.photopress.AppViewModel
 import net.c306.photopress.R
@@ -15,22 +17,32 @@ import net.c306.photopress.utils.viewBinding
  * Instances of this class are fragments representing a single object in our collection.
  */
 class WelcomeItemFragmentSelectBlog : Fragment(R.layout.fragment_welcome_item_select_blog) {
-    
+
     private val appViewModel by activityViewModels<AppViewModel>()
-    
-    private val viewModel by activityViewModels<SelectBlogViewModel>()
-    
+
+    private val viewModel by viewModels<SelectBlogViewModel>()
+
     private val binding by viewBinding(FragmentWelcomeItemSelectBlogBinding::bind)
-    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: 11/02/2024 Test this
+        setFragmentResultListener(SELECT_BLOG_REQUEST_KEY) { _, bundle ->
+            if (bundle.containsKey(SELECTED_BLOG_KEY)) {
+                viewModel.setSelectedBlogId(bundle.getInt(SELECTED_BLOG_KEY))
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        
+
         viewModel.blogsAvailable.observe(viewLifecycleOwner) {
             if (it == null) {
                 viewModel.refreshBlogsList()
             }
             binding.tvNoBlogMessage.isVisible = it == false
         }
-    
+
         appViewModel.blogSelected.observe(viewLifecycleOwner) {
             binding.animationViewDone.isVisible = it
             if (it == true) {
@@ -39,24 +51,29 @@ class WelcomeItemFragmentSelectBlog : Fragment(R.layout.fragment_welcome_item_se
             binding.groupSetupComplete.isVisible = it
             binding.buttonSelectBlog.isVisible = !it
         }
-        
+
         viewModel.selectBlogWelcomeSubtitle.observe(viewLifecycleOwner) {
             binding.subtitleWelcomeSelectBlog.text = it
         }
-        
+
         viewModel.blogList.observe(viewLifecycleOwner) {
             binding.buttonSelectBlog.isEnabled = !it.isNullOrEmpty()
         }
-        
+
         binding.buttonStart.setOnClickListener { goToApp() }
         binding.buttonSelectBlog.setOnClickListener { openBlogChooser() }
     }
-    
+
     private fun goToApp() {
         findNavController().navigate(WelcomeFragmentDirections.actionGoToApp())
     }
-    
+
     private fun openBlogChooser() {
         findNavController().navigate(WelcomeFragmentDirections.actionSelectBlog())
+    }
+
+    companion object {
+        const val SELECT_BLOG_REQUEST_KEY = "selectBlogRequestKey"
+        const val SELECTED_BLOG_KEY = "selectedBlogKey"
     }
 }
