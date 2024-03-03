45b1466bdc340a0257366a171a9e9665e3322bc2
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2024-03-03 22:15:37 +0000

    Add compose dependencies to module ":feature:welcome"

--- feature/welcome/build.gradle.kts
+++ feature/welcome/build.gradle.kts
@@ -6,9 +6,54 @@ plugins {
 
 android {
     namespace = "net.c306.photopress.welcome"
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.9"
+    }
 }
 
 java {
     sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
     targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+}
+
+dependencies {
+    implementation(libs.androidx.fragment.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+
+    // Choose one of the following:
+    // Material Design 3
+    implementation(libs.androidx.compose.material3)
+    // or only import the main APIs for the underlying toolkit systems,
+    // such as input and measurement/layout
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.utils)
+
+    // Android Studio Preview support
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // UI Tests
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
+    // Included automatically by material, only add when you need
+    // the icons but not the material library (e.g. when using Material3 or a
+    // custom design system based on Foundation)
+    implementation(libs.androidx.compose.material.iconsCore)
+    // Add full set of material icons
+    implementation(libs.androidx.compose.material.iconsExtended)
+    // Add window size utils
+    implementation(libs.androidx.compose.material3.windowSizeClass)
+
+    // Integration with activities
+    implementation(libs.androidx.activity.compose)
+    // Integration with ViewModels
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Integration with LiveData
+    implementation(libs.androidx.compose.runtime.livedata)
 }
