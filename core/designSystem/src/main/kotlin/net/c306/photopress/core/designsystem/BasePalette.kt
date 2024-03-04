6fc4252e57b14b804138b5159ee9f7d8ca717c58
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2024-03-04 16:51:03 +0000

    Add `BasePalette` with gray variants

---
+++ core/designSystem/src/main/kotlin/net/c306/photopress/core/designsystem/BasePalette.kt
@@ -0,0 +1,21 @@
+package net.c306.photopress.core.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Base colour palette used throughout the app.
+ * Does not provide light & dark mode variants.
+ */
+object BasePalette {
+    val GREY_50 = Color(0xFFFAFAFA)
+    val GREY_100 = Color(0xFFF5F5F5)
+    val GREY_200 = Color(0xFFEEEEEE)
+    val GREY_300 = Color(0xFFE0E0E0)
+    val GREY_400 = Color(0xFFBDBDBD)
+    val GREY_500 = Color(0xFF9E9E9E)
+    val GREY_600 = Color(0xFF757575)
+    val GREY_700 = Color(0xFF616161)
+    val GREY_800 = Color(0xFF424242)
+    val GREY_900 = Color(0xFF212121)
+    val GREY_950 = Color(0xFF121212)
+}
